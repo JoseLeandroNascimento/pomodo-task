0f9ad00d03b209073f1ff523980bb6e905478f9c
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-04-29 19:46:38 -0500

    fix: ajustando componente de date

--- app/src/main/java/com/example/pomodo_task/components/InputDate.kt
+++ app/src/main/java/com/example/pomodo_task/components/InputDate.kt
@@ -1,12 +1,19 @@
 package com.example.pomodo_task.components
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -14,18 +21,22 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pomodo_task.ui.theme.Green300
 import java.text.SimpleDateFormat
@@ -43,8 +54,16 @@ fun InputDate(
 
     var showModal by remember { mutableStateOf(false) }
     val datePikerState = rememberDatePickerState()
-    val dateFormat = remember {SimpleDateFormat("dd/MM/yyyy") }
+    val dateFormat = remember { SimpleDateFormat("dd/MM/yyyy") }
     var selectedText by remember { mutableStateOf("") }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    LaunchedEffect(isPressed) {
+        if (isPressed) {
+            showModal = !showModal
+        }
+    }
 
     OutlinedTextField(
         modifier = modifier,
@@ -63,7 +82,9 @@ fun InputDate(
         ),
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
         value = selectedText,
+        readOnly = true,
         onValueChange = { },
+        interactionSource = interactionSource,
         trailingIcon = {
             IconButton(onClick = {
                 showModal = !showModal
@@ -79,23 +100,48 @@ fun InputDate(
                 showModal = false
             },
             dismissButton = {
-                // Se quiser, adicione um botão de cancelar aqui
-            },
-            confirmButton = {
-                Button(onClick = {
-                    val millis = datePikerState.selectedDateMillis
-                    if (millis != null) {
-                        val date = Date(millis)
-                        selectedText = dateFormat.format(date)
-                        onValueChange(date)
-                    }
+                TextButton(onClick = {
                     showModal = false
                 }) {
-                    Text(text = "Confirmar")
+                    Text(text = "Cancelar", color = Green300)
+                }
+            },
+            colors = DatePickerDefaults.colors(
+                containerColor = if (isSystemInDarkTheme()) MaterialTheme.colorScheme.surface else Color.White,
+            ),
+            confirmButton = {
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Green300
+                    ),
+                    onClick = {
+                        val millis = datePikerState.selectedDateMillis
+                        if (millis != null) {
+                            val date = Date(millis)
+                            selectedText = dateFormat.format(date)
+                            onValueChange(date)
+                        }
+                        showModal = false
+                    }) {
+
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        Text(text = "Confirmar")
+                        Icon(imageVector = Icons.Default.Check, contentDescription = "Confirm")
+                    }
                 }
             }
         ) {
-            DatePicker(state = datePikerState)
+            DatePicker(
+                colors = DatePickerDefaults.colors(
+                    selectedDayContainerColor = Green300,
+                    todayDateBorderColor = Green300,
+                    selectedYearContainerColor = Green300,
+                ),
+                state = datePikerState
+            )
         }
     }
 
